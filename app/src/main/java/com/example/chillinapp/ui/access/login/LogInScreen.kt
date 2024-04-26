c369e37da674495eaddf4a2aba0a93dcdae611e4
EmanueleRsp <e.respino1@studenti.unipi.it>
2024-04-26 15:53:35 +0200

    Updated libs versions and elements

--- app/src/main/java/com/example/chillinapp/ui/access/login/LogInScreen.kt
+++ app/src/main/java/com/example/chillinapp/ui/access/login/LogInScreen.kt
@@ -24,7 +24,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Divider
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
