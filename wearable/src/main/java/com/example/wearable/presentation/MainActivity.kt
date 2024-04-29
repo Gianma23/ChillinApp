67684b44d2a1bad47f8d0af413553762f6bce7fd
Daniel Namaki <namaki.work@gmail.com>
2024-04-29 21:48:19 +0200

    add SynchronizationChannelHandler on wearable + removed BLE support

--- wearable/src/main/java/com/example/wearable/presentation/MainActivity.kt
+++ wearable/src/main/java/com/example/wearable/presentation/MainActivity.kt
@@ -6,19 +6,10 @@
 
 package com.example.wearable.presentation
 
-import android.Manifest
-import android.content.ComponentName
-import android.content.Context
 import android.content.Intent
-import android.content.ServiceConnection
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.os.IBinder
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -30,38 +21,15 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.TimeText
 import com.example.wearable.R
 import com.example.wearable.presentation.theme.ChillinAppTheme
-import com.example.wearable.synchronization.BLEService
+import com.example.wearable.synchronization.SynchronizationChannelHandler
 
 class MainActivity : ComponentActivity() {
-    private var bleService: BLEService? = null
-
-    private val serviceConnection = object : ServiceConnection {
-        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-            bleService = (service as BLEService.LocalBinder).service
-        }
-
-        override fun onServiceDisconnected(name: ComponentName?) {
-            bleService = null
-        }
-    }
-
-    val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
-        if (isGranted) {
-            // Permission is granted
-            Log.d("MainActivity", "Permission granted")
-
-        } else {
-            // Permission denied
-            Log.d("MainActivity", "Permission denied")
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
@@ -74,50 +42,26 @@ class MainActivity : ComponentActivity() {
             WearApp("Android")
         }
 
-        // val serviceIntent = Intent(this, BLEService::class.java)
-        // startService(serviceIntent)
-        checkPermissionNeeded()
-        Intent(this, BLEService::class.java).also { intent ->
-            startService(intent)
-            bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
-        }
+        val intent = Intent(this, SynchronizationChannelHandler::class.java)
+        intent.setAction("START")
+        startService(intent)
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        unbindService(serviceConnection)
+        // TODO: stop sensor services
+        /*
+        var intent = Intent(this, SensorHandler::class.java)
+        intent.setAction("stop_sensors")
+        startService(intent)
+         */
+
+        intent = Intent(this, SynchronizationChannelHandler::class.java)
+        intent.setAction("STOP")
+        startService(intent)
     }
 
-    private fun checkPermissionNeeded() {
-        val permissions = arrayOf(
-            Manifest.permission.BLUETOOTH_CONNECT,
-            Manifest.permission.BLUETOOTH_ADVERTISE
-        )
-        permissions.forEach { permission ->
-            when {
-                ContextCompat.checkSelfPermission(
-                    this,
-                    permission
-                ) == PackageManager.PERMISSION_GRANTED -> {
-                    // Permission is already granted
-                    Log.d("MainActivity", "Permission granted")
-                }
 
-                shouldShowRequestPermissionRationale(permission) -> {
-                    // Show an explanation to the user *asynchronously* -- don't block
-                    // this thread waiting for the user's response! After the user
-                    // sees the explanation, try again to request the permission.
-                    Log.d("MainActivity", "Permission denied, show rationale")
-                }
-
-                else -> {
-                    // No explanation needed; request the permission
-                    requestPermissionLauncher.launch(permission)
-                    Log.d("MainActivity", "Request permission")
-                }
-            }
-        }
-    }
 }
 
 @Composable
