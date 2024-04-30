cdfe0cfa8b6ace784dba0bdb5e723c62bcad276b
Daniel Namaki <namaki.work@gmail.com>
2024-04-30 12:09:56 +0200

    add bulk data support
    Defined 30 samples to be sent in a single communication between devices

--- app/src/main/java/com/example/chillinapp/synchronization/SensorData.java
+++ app/src/main/java/com/example/chillinapp/synchronization/SensorData.java
@@ -10,19 +10,16 @@ public class SensorData {
     private Long timestamp;
     private Double eda;
     private Double skinTemperature;
-    private Double heartRate;
 
 
     /**
      * Constructor for the SensorData class.
      * @param eda The Electrodermal Activity value.
      * @param skinTemperature The skin temperature value.
-     * @param heartRate The heart rate value.
      */
-    public SensorData(Double eda, Double skinTemperature, Double heartRate) {
+    public SensorData(Double eda, Double skinTemperature) {
         this.eda = eda;
         this.skinTemperature = skinTemperature;
-        this.heartRate = heartRate;
         this.timestamp = setCurrentTimestamp();
     }
 
@@ -31,12 +28,10 @@ public class SensorData {
      * @param timestamp The timestamp of the data.
      * @param eda The Electrodermal Activity value.
      * @param skinTemperature The skin temperature value.
-     * @param heartRate The heart rate value.
      */
-    public SensorData(Long timestamp, Double eda, Double skinTemperature, Double heartRate) {
+    public SensorData(Long timestamp, Double eda, Double skinTemperature) {
         this.eda = eda;
         this.skinTemperature = skinTemperature;
-        this.heartRate = heartRate;
         this.timestamp = timestamp;
     }
 
@@ -52,20 +47,12 @@ public class SensorData {
         return skinTemperature;
     }
 
-    public Double getHeartRate() {
-        return heartRate;
-    }
-
     public void setEda(Double eda) {
         this.eda = eda;
     }
 
     public void setSkinTemperature(Double skinTemperature) {
         this.skinTemperature = skinTemperature;
-    }
-
-    public void setHeartRate(Double heartRate) {
-        this.heartRate = heartRate;
     }
 
     public void setTimestamp(Long timestamp) {
@@ -86,7 +73,6 @@ public class SensorData {
                 "timestamp=" + timestamp +
                 ", eda=" + eda +
                 ", skinTemperature=" + skinTemperature +
-                ", heartRate=" + heartRate +
                 '}';
     }
 }
