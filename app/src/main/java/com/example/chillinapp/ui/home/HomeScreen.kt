09ae17af29018fa1366c7e18a4c53b9b1c0c1742
EmanueleRsp <e.respino1@studenti.unipi.it>
2024-05-09 12:14:35 +0200

    Minor fixes

--- app/src/main/java/com/example/chillinapp/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/chillinapp/ui/home/HomeScreen.kt
@@ -3,6 +3,7 @@ package com.example.chillinapp.ui.home
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -21,6 +22,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -28,6 +31,7 @@ import androidx.navigation.compose.rememberNavController
 import com.example.chillinapp.R
 import com.example.chillinapp.ui.navigation.HomeNavGraph
 import com.example.chillinapp.ui.navigation.NavigationDestination
+import com.example.chillinapp.ui.theme.ChillInAppTheme
 
 
 object HomeDestination : NavigationDestination {
@@ -62,7 +66,8 @@ fun HomeScreen(
         HomeNavGraph(
             homeNavController = homeNavController,
             mainNavController = navController,
-            modifier = Modifier.padding(innerPadding)
+            modifier = Modifier
+                .padding(horizontal = innerPadding.calculateStartPadding(LayoutDirection.Ltr))
         )
     }
 }
@@ -106,7 +111,10 @@ private fun HomeBottomBar(
     navController: NavHostController,
     items: List<Screen>
 ) {
-    BottomAppBar {
+    BottomAppBar(
+        contentColor = MaterialTheme.colorScheme.surface,
+        tonalElevation = 0.dp
+    ) {
         val navBackStackEntry by navController.currentBackStackEntryAsState()
         val currentRoute = navBackStackEntry?.destination?.route
 
@@ -125,5 +133,13 @@ private fun HomeBottomBar(
                 }
             )
         }
+    }
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    ChillInAppTheme {
+        HomeScreen(navController = rememberNavController())
     }
 }
